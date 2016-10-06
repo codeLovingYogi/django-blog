3c684aadaecb11de4808af01af2adba1350d5d88
codeLovingYogi <felichay@outlook.com>
2016-10-06 00:48:08 -0400

    Django REST framework tutorial, 2. Requests and Responses

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -15,3 +16,5 @@ urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
